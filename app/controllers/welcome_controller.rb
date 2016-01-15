4c4249c0672bdaecd2bbd93cd997c47c74ac4371
bessibes@gmail.com <bessibes@gmail.com>
2016-01-15 16:56:09 +0100

    product.search

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -7,8 +7,7 @@ class WelcomeController < ApplicationController
   end
 
   def search_on_product
-    products = Product.search_on_product(params[:term])
-    render json: cities.map{ |c| { :id => c._id, :value => "#{c.name}, #{c.region.name}, #{c.region.country.name}" } }
+    products = Product.search(params[:term])
   end
 
 end
